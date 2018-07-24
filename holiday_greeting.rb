6593a3a4240d621ea667273cd287610845b87428
samyuravikumar <samyuktha321@gmail.com>
2018-07-24 15:53:18 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,12 @@
+def holiday_greeting(recipient = "Mom", sender = "Your Favorite Child", holiday = "Mother's Day")
+  "Happy #{holiday}, #{recipient}! From #{sender}."
+end
+
+puts "Welcome! What holiday are you celebrating?"
+holiday = gets.strip
+puts "Who are you sending this to?"
+recipient = gets.strip
+puts "What is your name?"
+sender = gets.strip
+
+puts holiday_greeting(recipient, sender, holiday)
